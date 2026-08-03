9caf4eef8a4250734faa1e07780b0f5be0865232
Jimmy Hoang <jxnzp1@gmail.com>
2026-08-03 02:16:37 -0600

    Updated collection to dynamically create fragrances instead of manually

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod database;
 mod models;
 slint::include_modules!();
+use slint::{ModelRc, VecModel};
 
 fn main() {
     let database = database::establish_connection();
@@ -14,6 +15,27 @@ fn main() {
     println!("{:#?}", fragrances);
 
     let ui = MainWindow::new().unwrap();
+
+    let fragrances = database::fragrance_repository::get_all(&database);
+
+    let ui_fragrances = fragrances
+        .iter()
+        .map(|f| {
+            FragranceData {
+                brand: f.brand.clone().into(),
+                name: f.name.clone().into(),
+                rating: f.rating,
+                notes: f.notes.clone().into(),
+                seasons: f.seasons.clone().into(),
+            }
+        }).collect::<Vec<_>>();
+
+
+    ui.set_fragrances(
+        ModelRc::new(
+            VecModel::from(ui_fragrances)
+        )
+    );
 
     ui.run().unwrap();
 }
